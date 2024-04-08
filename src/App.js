bd3e965d76ce9cb640c3fa50135bafec91a22a0c
Matias Vazquez <vazquezmatiasm@gmail.com>
2024-04-08 11:55:03 -0300

    cambio foto genesys

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     {
       id: uuid(),
       equipo: "Programación",
-      foto: "https://github.com/genesysaluralatam.png",
+      foto: "https://github.com/genesysR-dev.png",
       nombre: "Genesys Rondón",
       puesto: "Desarrolladora de software e instructora",
       fav: false
